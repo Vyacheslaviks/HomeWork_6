45d309e403b432bece27f6e3181001f7f1e6e5b5
Slava <slavanew98@mail.ru>
2022-12-11 20:36:13 +0300

    1st exercise - completed

--- Exc_1/Program.cs
+++ Exc_1/Program.cs
@@ -1,2 +1,62 @@
-﻿GCNotificationStatus // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Тело программы
+
+Console.Write("Введите кол-во элементов массива: ");
+int M = InputInt();
+
+int[] local_array = FillArray(M);
+PrintArray(local_array);
+
+Console.WriteLine($"Кол-во положительных чисел = {CountNegativeNumber(local_array)}");
+
+//Методы
+
+int InputInt()
+{
+    bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+    if(isParse)
+    {
+        return number;
+    }
+    else
+    {
+        throw new ArgumentException("Введено некорректое значение!");
+    }
+}
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"введите значение для {i} элемента: ");
+        array[i] = InputInt();
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    string out_array = string.Join(", ", array);
+    Console.WriteLine($"[{out_array}]");
+}
+
+int CountNegativeNumber(int[] array)
+{
+    int count = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
